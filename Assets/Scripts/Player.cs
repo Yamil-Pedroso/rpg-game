16d7b34d709ff23cef60b41860db288b5d7d5ed0
Yami Pedroso-AI <yamirovinci@gmail.com>
2024-11-14 10:57:24 +0100

    Adding properties to our basic character

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rb;
+
+    public float moveSpeed;
+    public float jumpForce;
+    public float xInput;
     void Start()
     {
-        
+        //rb.linearVelocity = new Vector2(0, 10); 
     }
 
     void Update()
     {
-        
+        xInput = Input.GetAxisRaw("Horizontal"); 
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            //Debug.Log("Jump!");
+        }
     }
 }
